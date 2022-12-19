495d2ca483db8ec7aef1952b00fe10abceafaad5
JavaMaster <71312371+progagoda@users.noreply.github.com>
2022-12-19 22:26:12 +0300

    change misstakes

--- 1-partDB.sql
+++ 1-partDB.sql
@@ -7,7 +7,7 @@ CREATE TABLE Order_for_drive(
     FOREIGN KEY(driver_id)
     REFERENCES driver(id),
     FOREIGN KEY(farmer_id)
-    REFERENCES farmer(id),
+    REFERENCES farmer(id)
 );
 CREATE TABLE Country(
     name varchar(30) PRIMARY KEY,
@@ -22,14 +22,14 @@ CREATE TABLE Location(
     price_per_month int,
     square int CHECK(square>0),
     FOREIGN KEY(country_name)
-    REFERENCES country(name),
+    REFERENCES country(name)
 );
 CREATE INDEX idx_price_per_month on Location(price_per_month);
 CREATE TABLE Farm(
    id PRIMARY KEY,
    location_id int NOT NULL,
        FOREIGN KEY(location_id)
-    REFERENCES Location(id),
+    REFERENCES Location(id)
 );
 
 CREATE TABLE Farmer(
@@ -39,8 +39,8 @@ CREATE TABLE Farmer(
     farm_id int,
     FOREIGN KEY(farmer_inf_login)
     REFERENCES user(login),
-    FOREIGN KEY(farm_id),
-    REFERENCES farm(id),
+    FOREIGN KEY(farm_id)
+    REFERENCES farm(id)
 );
 CREATE TABLE Farmer_orders(
    id PRIMARY KEY,
@@ -49,36 +49,36 @@ CREATE TABLE Farmer_orders(
        FOREIGN KEY(fermer_id)
     REFERENCES fermer(id),
      FOREIGN KEY(order_id)
-    REFERENCES order(id),
+    REFERENCES order(id)
 );
 CREATE TABLE Customer(
    id PRIMARY KEY,
    name varchar(10) NOT NULL,
    phone int(10) NOT NULL UNIQUE,
-   mail varchar(50),
+   mail varchar(50)
 );
 CREATE TABLE Review(
    id PRIMARY KEY,
    sendler_id int NOT NULL,
    message varchar(255) NOT NULL,
-   rate int CHECK(rate>0 AND rate<5),
+   rate int CHECK(rate>0 AND rate<5)
 );
 CREATE INDEX idx_rate on Review(rate);
 CREATE TABLE User(
    login varchar(10) PRIMARY KEY,
    phone int(10) NOT NULL UNIQUE,
    mail varchar(50) NOT NULL UNIQUE,
-   password varchar(15) NOT NULL,
+   password varchar(15) NOT NULL
 );
 
 CREATE TABLE Review_list(
    id PRIMARY KEY,
    rewiew_id int  NOT NULL,
    user_login varchar(10) NOT NULL,
-   FOREIGN KEY(rewiew_id),
+   FOREIGN KEY(rewiew_id)
     REFERENCES Review(id),
      FOREIGN KEY(user_login)
-    REFERENCES User(id),
+    REFERENCES User(id)
 );
 CREATE TABLE Arbitration(
    id PRIMARY KEY,
